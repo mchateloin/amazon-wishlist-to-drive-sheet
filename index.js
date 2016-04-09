05c7426a18065b10229b9a2066e121a749964cd3
Miguel Chateloin <mchateloin@gmail.com>
2016-04-09 19:58:49 -0400

    Working prototype with reliance on local JSON file.

---
+++ index.js
@@ -0,0 +1,39 @@
+var RSVP = require('rsvp'),
+    utils = require('./lib')
+    fs = require('fs'),
+    GoogleSpreadsheet = require("google-spreadsheet");
+    config = JSON.parse(fs.readFileSync('config.json', 'utf8')),
+    doc = new GoogleSpreadsheet(config['google_spreadsheet_key']);
+
+
+RSVP.hash({
+        currentItems: utils.getWishlistFromAmazon(config['amazon_wishlist_id']),
+        previousItems: utils.getPreviousWishlist(),
+        spreadsheetAuthenticated: utils.authenticateServiceAccount(doc, {
+            private_key: config['google_private_key'],
+            client_email: config['google_client_email']
+        })
+    })
+    .then(function(wishlist){
+        var itemsAdded = utils.getDifference(wishlist.previousItems, wishlist.currentItems),
+            rowValues = itemsAdded.map(function(item){
+                return {
+                    Image: '=IMAGE("' + item.picture + '")',
+                    Title: '=HYPERLINK("' + item.link + '", "' + item.name + '")'
+                };
+            }),
+            rowAddPromises = rowValues.map(function(rowObj){
+                return utils.addRowsToDriveSpreadsheet(doc, 1, rowObj);
+            });
+
+        rowAddPromises.push(utils.savePreviousWishlist(wishlist.currentItems));
+
+        return RSVP.all(rowAddPromises);
+    }, function(){
+        console.log('Error finding wishlists or authenticating to drive!')
+    })
+    .then(function(){
+        console.log('Success adding rows to spreadsheet!');
+    }, function(){
+        console.log('Error adding rows to spreadsheet!');
+    });
---
+++ lib.js
@@ -0,0 +1,94 @@
+var RSVP = require('rsvp'),
+    https = require('https'),
+    config = JSON.parse(fs.readFileSync('config.json', 'utf8')),
+    amazonToDriveKey = config['amazon_wishlist_id'] + '-' + config['google_spreadsheet_key'];
+
+module.exports = {
+
+    getWishlistFromAmazon: function(wishlistId){
+        return new RSVP.Promise(function(resolve, reject){
+            https.get({
+                host: 'amazon-wish-lister.herokuapp.com',
+                path: '/src/wishlist.php?id=' + wishlistId
+            }, function(response){
+
+                var body = '';
+
+                response.on('data', function(d) {
+                    body += d;
+                });
+
+                response.on('end', function() {
+                    resolve(JSON.parse(body));
+                }).on('error', function(error){
+                    reject(error);
+                })
+            })
+        });
+    },
+
+    getPreviousWishlist: function(){
+        return new RSVP.Promise(function(resolve, reject){
+            fs.readFile(amazonToDriveKey + '.json', 'utf8', function(err, data){
+                if (err) {
+                    resolve([]);
+                } else {
+                    resolve(JSON.parse(data));
+                }
+            });
+        });
+    },
+
+    savePreviousWishlist: function(wishlist){
+        return new RSVP.Promise(function(resolve, reject){
+             fs.writeFile(amazonToDriveKey + '.json', 'utf8', JSON.stringify(wishlist), function(err){
+                 if(err){
+                    reject(err);
+                 } else {
+                    resolve();
+                 }
+             });
+        })
+    },
+
+    authenticateServiceAccount: function(doc, creds){
+        return new RSVP.Promise(function(resolve, reject) {
+            doc.useServiceAccountAuth(creds, function (err) {
+                if(err){
+                    reject();
+                    return;
+                }
+
+                resolve();
+            })
+        });
+    },
+
+    addRowsToDriveSpreadsheet: function(doc, sheetIndex, rowObj){
+        return new RSVP.Promise(function(resolve, reject){
+            doc.addRow(sheetIndex, rowObj, function(err){
+
+                if(err){
+                    reject();
+                    return;
+                }
+
+                resolve();
+
+            });
+        });
+    },
+
+    getDifference: function(wishlistA, wishlistB){
+        var lookupForA = {};
+
+        wishlistA.forEach(function(item){
+            lookupForA[item.ASIN] = true;
+        });
+
+        return wishlistB.filter(function(itemFromB){
+            return typeof lookupForA[itemFromB.ASIN] === 'undefined';
+        });
+    }
+}
+
